851cecaba410f81a016055ff381c88ff3ebb7926
pospisilfilip <filippospisil64@gmail.com>
2025-11-24 09:31:21 +0100

    Add student selection and chat logging functionality

---
+++ Kody/24_11.py
@@ -0,0 +1,24 @@
+import random
+
+cesta = r"2. prace_se_soubory\data\studenti.txt"
+
+
+vybrani_studenti = []
+
+
+with open(cesta, "r", encoding="utf-8") as file:
+    studenti = file.readlines()
+
+    while len(vybrani_studenti) < 5:
+        novy_student = random.choice(studenti)
+
+        if novy_student not in vybrani_studenti:
+            vybrani_studenti.append(novy_student.strip())
+
+vybrani_studenti.sort()
+
+print(vybrani_studenti)
+
+with open("zkouseni_studenti.txt", "w", encoding="utf-8") as file:
+    for s in vybrani_studenti:
+        file.write(s+"\n")
